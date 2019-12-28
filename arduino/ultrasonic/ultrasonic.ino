f96c4dc8c89c7aba6791c8df6fde83e019026b97
fownten <fonteyn.hans@gmail.com>
2019-12-28 23:11:08 +0100

    Update ultrasonic.ino

--- arduino/ultrasonic/ultrasonic.ino
+++ arduino/ultrasonic/ultrasonic.ino
@@ -3,12 +3,20 @@
 
 void setup() {
   Serial.begin(9600);
- }
+}
  
  void loop() {
   
- }
+}
  
- long echo(void) {
- 
- }
+long echo(void) {
+  long duration;
+  pinMode(pingPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin,LOW);
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+  return microsecondsToCentimeters(duration);
+}
